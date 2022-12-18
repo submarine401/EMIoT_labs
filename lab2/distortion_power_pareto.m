c70ce118603e034ad68e97e388d2507aa8232c1a
Diamante Simone Crescenzo <s295783@studenti.polito.it>
2022-12-18 12:56:47 +0100

    feat: added support for 256x256 pixels images to distortion_power_pareto.m script chore: code cleaning and comments done on distortion_power_pareto.m

--- lab2/distortion_power_pareto.m
+++ lab2/distortion_power_pareto.m
@@ -8,50 +8,89 @@
 %compute number of images in the directory
 a = dir(['images.tar' '/*.tiff']);
 n=numel(a);
-A = zeros(512,512,3,15);
-B = zeros(512,512,3);
-p_array=zeros(8,10);
-dist_array = zeros(8,10);
+% data structures for 256x256 images
+A_256 = zeros(256,256,3,15);
+B_256 = zeros(256,256,3);
+p_array_256=zeros(8,10);
+dist_array_256 = zeros(8,10);
+% data structures for 512x512 images
+A_512 = zeros(512,512,3,8);
+B_512 = zeros(512,512,3);
+p_array_512=zeros(8,10);
+dist_array_512 = zeros(8,10);
 
-%store RGB representation of all images in an array
-
+i_str = 'images.tar/';
 for i = 1:8
-    i_str = num2str(i+7);
-    C= [i_str,'.tiff'];
-    A(:,:,:,i) = imread(C);
-    A=cast(A,'uint8');
+    %store RGB representation of all images in an array
+    if i<8  % 256x256 images are only 7, skip last iteration
+        image_str= [i_str, num2str(i), '.tiff'];
+        A_256(:,:,:,i) = imread(image_str);
+        A_256=uint8(A_256);
+    end
+    image_str= [i_str, num2str(i+7), '.tiff'];
+    A_512(:,:,:,i) = imread(image_str);
+    A_512=uint8(A_512);
     
+    %apply the blue reduction on the image
     j=1;
 
-   for k = 0.1:0.1:1
-   B = image_reduce_blue(A(:,:,:,i),k);
-   %compute power and distortion
-   p_img = image_power(A(:,:,:,i));
-   p_reduced = image_power(B); 
-   p_diff = p_img - p_reduced;
+    for k = 0.1:0.1:1
+        B_256 = image_reduce_blue(A_256(:,:,:,i),k);
+        %compute power and distortion for 256x256 images
+        p_img_256 = image_power(A_256(:,:,:,i));
+        p_reduced_256 = image_power(B_256); 
+        p_diff_256 = p_img_256 - p_reduced_256;
 
-   %store into the power array the normalized difference
-   p_array(i,j) = (p_diff*100)/p_img;
-   dist_array(i,j) = image_distortion(A(:,:,:,i),B);
-   j= j+1;
+        B_512 = image_reduce_blue(A_512(:,:,:,i),k);
+        %compute power and distortion for 512x512 images
+        p_img_512 = image_power(A_512(:,:,:,i));
+        p_reduced_512 = image_power(B_512); 
+        p_diff_512 = p_img_512 - p_reduced_512;
+        
+        %store into the power array the normalized difference 256x256
+        p_array_256(i,j) = (p_diff_256*100)/p_img_256;
+        dist_array_256(i,j) = image_distortion(A_256(:,:,:,i),B_256);
 
-   end
+        %store into the power array the normalized difference 512x512
+        p_array_512(i,j) = (p_diff_512*100)/p_img_512;
+        dist_array_512(i,j) = image_distortion(A_512(:,:,:,i),B_512);
+
+        j= j+1;
+    
+    end
 end
 
-%apply the blue reduction on the image
-j = 1; 
-plot(dist_array(1,:),p_array(1,:),'-or');
+% plot distortion vs power saving graph 256x256
+plot(dist_array_256(1,:),p_array_256(1,:),'-or');
 hold on
-plot(dist_array(2,:),p_array(2,:),'-o','Color','#7E2F8E');
-plot(dist_array(3,:),p_array(3,:),'-og');
-plot(dist_array(4,:),p_array(4,:),'-om');
-plot(dist_array(5,:),p_array(5,:),'-oc');
-plot(dist_array(6,:),p_array(6,:),'-ob');
-plot(dist_array(7,:),p_array(7,:),'-oy');
-plot(dist_array(8,:),p_array(8,:),'-ok');
+plot(dist_array_256(2,:),p_array_256(2,:),'-o','Color','#7E2F8E');
+plot(dist_array_256(3,:),p_array_256(3,:),'-og');
+plot(dist_array_256(4,:),p_array_256(4,:),'-om');
+plot(dist_array_256(5,:),p_array_256(5,:),'-oc');
+plot(dist_array_256(6,:),p_array_256(6,:),'-ob');
+plot(dist_array_256(7,:),p_array_256(7,:),'-oy');
 xlabel('Distortion (%)');
 ylabel('Power savings (%)');
-title('Power savings vs distortion pareto graph');
+title('Power savings vs distortion pareto graph 256x256');
+lgd = legend('1','2','3','4','5','6','7');
+title(lgd, 'Images (.tiff format)');
+grid on
+hold off
+
+% plot distortion vs power saving graph 512x512
+figure
+plot(dist_array_512(1,:),p_array_512(1,:),'-or');
+hold on
+plot(dist_array_512(2,:),p_array_512(2,:),'-o','Color','#7E2F8E');
+plot(dist_array_512(3,:),p_array_512(3,:),'-og');
+plot(dist_array_512(4,:),p_array_512(4,:),'-om');
+plot(dist_array_512(5,:),p_array_512(5,:),'-oc');
+plot(dist_array_512(6,:),p_array_512(6,:),'-ob');
+plot(dist_array_512(7,:),p_array_512(7,:),'-oy');
+plot(dist_array_512(8,:),p_array_512(8,:),'-ok');
+xlabel('Distortion (%)');
+ylabel('Power savings (%)');
+title('Power savings vs distortion pareto graph 512x512');
 lgd = legend('8','9','10','11','12','13','14','15');
 title(lgd, 'Images (.tiff format)');
 grid on
