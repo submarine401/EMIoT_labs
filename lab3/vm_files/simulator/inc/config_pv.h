edb050abe1b31d9227a98f7ecd3d98e22e34721e
submarine401 <salta.alessio@gmail.com>
2023-01-16 09:52:34 +0100

    feat: populated arrays in PV, PV_conv and battery_conv config files

--- lab3/vm_files/simulator/inc/config_pv.h
+++ lab3/vm_files/simulator/inc/config_pv.h
@@ -1,5 +1,5 @@
 #define TRACE_PERIOD 900
 #define SIZE_PV 4 
-static const double G[SIZE_PV] = {TO-BE-POPULATED};
-static const double I_MPP[SIZE_PV] = {TO-BE-POPULATED};
-static const double V_MPP[SIZE_PV] = {TO-BE-POPULATED};
+static const double G[SIZE_PV] = {250, 500, 750, 1000};
+static const double I_MPP[SIZE_PV] = {13.667, 28.1109, 41.2965, 57.1429};
+static const double V_MPP[SIZE_PV] = {2.9027, 2.9891, 3.1715, 3.1533};
