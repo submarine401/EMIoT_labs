1465479eec78e948a08a245ed0226d4e59207dbf
Diamante Simone Crescenzo <s295783@studenti.polito.it>
2023-01-17 16:20:49 +0100

    chore: minor comment fix

--- lab3/vm_files/simulator/src/battery_voc.cpp
+++ lab3/vm_files/simulator/src/battery_voc.cpp
@@ -44,12 +44,12 @@ void battery_voc::processing()
 
     // SOC and battery Voc relationship
     //v_oc.write(TO-BE-FILLED);   // clean template
-    v_oc.write(3.3*exp(0.2125*tmpsoc)-2.566*exp(-60.34*tmpsoc));   // EMPONENTIAL with 2 terms
-    //v_oc.write(1.1668*pow(tmpsoc,3)-2.415*pow(tmpsoc,2)-2.566*tmpsoc-60.34);   // POLYNOMIAL 3rd degree p1*x^3 + p2*x^2 + p3*x + p4
+    v_oc.write(3.3*exp(0.2125*tmpsoc)-2.566*exp(-60.34*tmpsoc));   // EXPONENTIAL with 2 terms: a*exp(b*x) + c*exp(d*x)
+    //v_oc.write(1.1668*pow(tmpsoc,3)-2.415*pow(tmpsoc,2)-2.566*tmpsoc-60.34);   // POLYNOMIAL 3rd degree: p1*x^3 + p2*x^2 + p3*x + p4
     //v_oc.write(387.5/(pow(tmpsoc,4)-1.163*pow(tmpsoc,3)-2.094*pow(tmpsoc,2)-4.697*tmpsoc+106.4));   // RATIONAL function: (p1) / (x^4 + q1*x^3 + q2*x^2 + q3*x + q4)
 
     // SOC and battery internal resistance relationship
-    //r_s.write(TO-BE-FILLED); // Place interpolated funct here
+    //r_s.write(TO-BE-FILLED); // clean template
     r_s.write(0.1239*exp(-1.55*tmpsoc)+0.007535*exp(1.52*tmpsoc)); // EXPONENTIAL with 2 terms: a*exp(b*x) + c*exp(d*x)
     //r_s.write(-0.02748*pow(tmpsoc,3)+0.1318*pow(tmpsoc,2)-0.1751*tmpsoc+0.1312);   // POLYNOMIAL 3rd degree: p1*x^3 + p2*x^2 + p3*x + p4
     //r_s.write(-0.1488*pow(tmpsoc,0.2192)+0.2031);   // POWER with 2 terms: a*x^b+c
