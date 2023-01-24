52ceeca5f200747ad162ad087f915e7ed136264b
submarine401 <salta.alessio@gmail.com>
2023-01-24 12:57:38 +0100

    feat:added small scripts for plotting simulated data

--- lab3/scripts/parallel_sim_plots.m
+++ lab3/scripts/parallel_sim_plots.m
@@ -1,43 +1,40 @@
 %% script for plotting data coming from the "parallel" simulation
 
-plot(time1,soc1,'.-')
+plot(time,soc,'.-')
 hold on
 xlabel("Time(s)");
 ylabel("Battery SOC")
-title("time vs SOC - time vs V_batt")
+title("time vs SOC - time vs V_{batt}")
 
 yyaxis right
-plot(time1,v_batt,'.-')
+plot(time,v_batt,'.-')
 ylabel("Battery voltage (V)");
-legend("SOC","V_batt");
+legend("SOC","V_{batt}");
 
 hold off
 
 figure(2)
-plot(time1,v_batt,'.-')
+plot(time,v_batt,'.-')
 hold on
 xlabel("Time(s)");
 ylabel("Battery voltage (V)")
-title("time vs SOC - time vs V_batt")
+title("time vs V_{batt} - time vs V_{PV}")
 
 yyaxis right
-plot(time1,v_pv,'.-')
+plot(time,v_pv,'.-')
 ylabel("PV panel voltage (V)");
 legend("V_{batt}","V_{PV}");
 
 hold off
 
 figure(3)
-plot(time1,i_pv,'.-g')
+plot(time,i_pv,'.-g')
 hold on
-plot(time1,real_i_pv,'.-b')
-% plot(time1,i_mcu,'.-k')
-% plot(time1,i_pv,'.-c')
-% plot(time1,i_rf,'.-y')
-% plot(time1,i_air_quality_sensor,'Color',"#EDB120")
-% plot(time1,i_methane_sensor,'Color','#A2142F')
-% plot(time1,i_temperature_sensor,'Color','#D81DBF')
 title('PV panel current vs real PV panel current')
 xlabel('Time')
 ylabel('Current(mA)')
-legend('I_{pv}','Real I_{pv}')
+axes('Position',[.7 .7 .2 .2])
+box on
+plot(time(85000:134000),real_i_pv(85000:134000),'.-b') %for standard
+%parallel sim
+%plot(time,real_i_pv,'.-b')
