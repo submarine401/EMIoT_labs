c2f9e74eded17739f0561a2d4f1e9bea7f8f9f86
submarine401 <salta.alessio@gmail.com>
2023-01-24 15:50:18 +0100

    feat: added support for two PV panels in series/parallel

--- lab3/vm_files/simulator/inc/config_pv.h
+++ lab3/vm_files/simulator/inc/config_pv.h
@@ -2,4 +2,12 @@
 #define SIZE_PV 4 
 static const double G[SIZE_PV] = {250, 500, 750, 1000};
 static const double I_MPP[SIZE_PV] = {13.667, 28.1109, 41.2965, 57.1429};
-static const double V_MPP[SIZE_PV] = {2.9027, 2.9891, 3.1715, 3.1533};
+//static const double V_MPP[SIZE_PV] = {2.9027, 2.9891, 3.1715, 3.1533};
+
+//======ALTERNATIVE CONFIGURATIONS (SERUES/PARALLEL)=====
+
+//MPP voltage with two panels in series
+static const double V_MPP[SIZE_PV] = {5.8054, 5.9782, 6.343, 6.3066};
+
+//MPP current with two panels in parallel
+//static const double I_MPP[SIZE_PV] = {27.334, 56.2218, 82.593, 114.2858};
